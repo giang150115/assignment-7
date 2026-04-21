be76ca7c8b1efd08959b778b4e973c038fa5ac83
giang150115 <hanguyentruonggiang2004@gmail.com>
2026-04-21 21:12:44 +0700

    Update baitap2 main.c

--- baitap2 main.c
+++ baitap2 main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include "hashtable.h"
 
+int main() {
+    init_table();
+    
+    insert("An", "111");
+    insert("Binh", "222");
+    insert("Chi", "333");
+    
+    printf("--- Kiem tra tim kiem ---\n");
+    search("An");
+    search("Ngoan"); // Khong ton tai
+    
+    printf("--- Sau khi xoa Binh ---\n");
+    delete("Binh");
+    displayAll();
+    
+    free_table();
+    return 0;
+}
