596e5cfd6e520a862473fde6b49c9bf4979092fe
giang150115 <hanguyentruonggiang2004@gmail.com>
2026-04-21 21:12:27 +0700

    Update baitap2 hashtable.h

--- baitap2 hashtable.h
+++ baitap2 hashtable.h
@@ -1,1 +1,24 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
 
+#define TABLE_SIZE 10
+
+typedef struct Node {
+    char name[50];
+    char phone[15];
+    struct Node* next;
+} Node;
+
+// Khai báo mảng buckets (external)
+extern Node* buckets[TABLE_SIZE];
+
+// Nguyên mẫu các hàm
+void init_table();
+unsigned int hash(char *str);
+void insert(char *name, char *phone);
+void search(char *name);
+void delete(char *name);
+void free_table();
+void displayAll();
+
+#endif
